2b2f176701b4b698bf7e63184c18cbda9e0f37a6
albaam00 <61425777+albaam00@users.noreply.github.com>
2022-01-23 12:52:17 +0100

    Update Creativa2.py

--- Creativa2.py
+++ Creativa2.py
@@ -2,15 +2,13 @@ from subprocess import call
 import os
 import sys
 from os import remove
-import webbrowser
+#import webbrowser
 
 #clonar aplicación
 call(["git clone https://github.com/CDPS-ETSIT/practica_creativa2.git",  "-y"], shell=True)
 
 
 #instalación pip 
-#call(["sudo apt-get install pip",  "-y"], shell=True)
-#call(["sudo apt-get install python3-pip",  "-y"], shell=True)
 call(["curl -O https://bootstrap.pypa.io/get-pip.py",  "-y"], shell=True)
 call(["python3 get-pip.py",  "-y"], shell=True)
 
@@ -38,23 +36,7 @@ fout.close()
 
 call(["mv","productpage2.html","practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
 
-'''
-os.environ['GROUP_NUMBER']="3"
-fin = open("practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r') # in file
-fout = open("productpage2.html", 'w') # out file
-for line in fin:
-    if "      <a class=\"navbar-brand\" href=\"#\">BookInfo Sample</a>" in line:
-        fout.write("      <a class=\"navbar-brand\" href=\"#\"> Equipo: "+str(os.environ.get('GROUP_NUMBER'))+"</a>")
-    else :
-        fout.write(line)
-fin.close()
-fout.close()
-
-call(["mv","productpage2.html","practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
-'''
-
-
 #Arrancar la aplicación
 os.chdir("practica_creativa2/bookinfo/src/productpage")
-webbrowser.open('http://192.168.122.1:9080/productpage')
+#webbrowser.open('http://192.168.122.1:9080/productpage')
 call(["python3 productpage_monolith.py 9080",  "-y"], shell=True)
